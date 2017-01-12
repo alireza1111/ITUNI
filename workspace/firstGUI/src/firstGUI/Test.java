f8dc90e49a95cdd100ac2aae9a2de8e607ab0e9d
Alireza D <alidav@chalmers.se>
2017-01-12 14:40:06 +0100

    12012017

---
+++ workspace/firstGUI/src/firstGUI/Test.java
@@ -0,0 +1,61 @@
+package firstGUI;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+
+public class Test {
+
+	private JFrame frame;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Test window = new Test();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Test() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JButton btnNewButton = new JButton("OK");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			textField.setText("Hello World!");	
+			}
+		});
+		frame.getContentPane().add(btnNewButton, BorderLayout.SOUTH);
+		
+		textField = new JTextField();
+		frame.getContentPane().add(textField, BorderLayout.NORTH);
+		textField.setColumns(10);
+	}
+
+}
